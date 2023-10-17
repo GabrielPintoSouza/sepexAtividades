40e0b8d74264a423fae3d8eb4916fee59c8daef9
Your Name <you@example.com>
2023-10-17 14:31:38 -0300

    Implementação da leitura de dados

--- CalculadoraBasica/main.c
+++ CalculadoraBasica/main.c
@@ -3,6 +3,14 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char operacao;
+    int numero1, numero2;
+    printf("Bem vindo ao programa Calculadora Basica:/n");
+    printf("Forneca a operacao que sera utilizada: + (soma), - (subtração), * (multiplicação), / (divisão)/n");
+    scanf("%c", &operacao);
+    printf("Forneca o primeiro numero inteiro:/n");
+    scanf("%d", &numero1);
+    printf("Forneca o segundo numero inteiro:/n");
+    scanf("%d", &numero2);
     return 0;
 }
