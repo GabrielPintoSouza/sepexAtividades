19a03d20ca867b5242f37d536cc9e25cd1c511a5
Your Name <you@example.com>
2023-10-17 15:19:47 -0300

    Implementação da estrutura de decisao e impressão do resultado final

--- CalculadoraBasica/main.c
+++ CalculadoraBasica/main.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int soma (int n1, int n2){
-    return n1 + n2;
+float soma (int n1, int n2){
+    float resultado;
+    resultado = n1+n2;
+    return resultado;
 }
 
-int subtracao (int n1, int n2){
-    return n1 - n2;
+float subtracao (int n1, int n2){
+    float resultado;
+    resultado = n1 - n2;
+    return resultado;
 }
 
-int multiplicacao (int n1, int n2){
-    return n1*n2;
+float multiplicacao (int n1, int n2){
+    float resultado;
+    resultado = n1 * n2;
+    return resultado;
 }
 
 float divisao (int n1, int n2){
-    return n1/n2;
+    float resultado;
+    resultado = n1 / n2;
+    return resultado;
 }
 
 int main()
 {
     char operacao;
     int numero1, numero2;
-    printf("Bem vindo ao programa Calculadora Basica:/n");
-    printf("Forneca a operacao que sera utilizada: + (soma), - (subtração), * (multiplicação), / (divisão)/n");
+    float resultado;
+    printf("Bem vindo ao programa Calculadora Basica:\n");
+    printf("Forneca a operacao que sera utilizada: + (soma), - (subtração), * (multiplicação), / (divisão)\n");
     scanf("%c", &operacao);
-    printf("Forneca o primeiro numero inteiro:/n");
+    printf("Forneca o primeiro numero inteiro:\n");
     scanf("%d", &numero1);
-    printf("Forneca o segundo numero inteiro:/n");
+    printf("Forneca o segundo numero inteiro:\n");
     scanf("%d", &numero2);
-    return 0;
+
+    switch(operacao){
+        case '+': resultado = soma(numero1, numero2);
+        break;
+        case '-': resultado = subtracao(numero1, numero2);
+        break;
+        case '*': resultado = multiplicacao(numero1, numero2);
+        break;
+        case '/': resultado = divisao(numero1, numero2);
+        break;
+        default: resultado = 0;
+    }
+
+    printf("O resultado da operacao foi: %f\n", resultado);
 }
